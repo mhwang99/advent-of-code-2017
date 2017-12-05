c8f71ab7a935a368d1ddb4281444692478da44d0
Mark Hwang <mhwang@liveopscloud.com>
2017-12-05 10:27:20 -0400

    day 5

--- src/aoc17/d5.clj
+++ src/aoc17/d5.clj
@@ -8,21 +8,22 @@
            n 0]
       (if (>= i cnt)
         n
-        (let [in (nth l i)]
-          (recur (update l i (fi in))
-                 (+ i in)
+        (let [o (nth l i)]
+          (recur (update l i (fi o))
+                 (+ i o)
                  (inc n)))))))
 
 (defn q1 [l]
   (q0 l (fn [_] inc)))
 
 (defn q2 [l]
-  (q0 l (fn [n] (if (>= n 3) dec inc))))
+  (q0 l #(if (>= % 3) dec inc)))
 
 (def in
   (mapv #(Integer. %)
         (-> (slurp "resources/d5.txt")
             (s/split #"\n"))))
 
-(q1 in)
-(q2 in)
+(defn run []
+  (println "q1" (q1 in))
+  (println "q2" (q2 in)))
