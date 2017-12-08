db7c7571243b39b7177d19a72210a26aaab07617
Mark Hwang <mhwang@serenova.com>
2017-12-08 01:59:27 -0400

    day 8

--- src/aoc17/d8.clj
+++ src/aoc17/d8.clj
@@ -2,44 +2,36 @@
   (:require [clojure.string :as s]))
 
 (defn q0 [ll]
-  (reduce (fn [[m mx] [dest op v src cm cmv]]
-            (if (cm (get m src 0) cmv)
-              (let [nv (op (get m dest 0) v)]
-                [(assoc m dest nv) (max mx nv)])
+  (reduce (fn [[m mx] [a fa va b fb vb]]
+            (if (fb (get m b 0) vb)
+              (let [nv (fa (get m a 0) va)]
+                [(assoc m a nv) (max mx nv)])
               [m mx]))
           [{} 0] ll))
 
 (defn q1 [ll]
-   (let [[m _] (q0 ll)]
-     (apply max (vals m))))
+  (let [[m _] (q0 ll)]
+    (apply max (vals m))))
 
 (defn q2 [ll]
-   (let [[_ mx] (q0 ll)]
-     mx))
+  (let [[_ mx] (q0 ll)]
+    mx))
 
-(defn parse [s]
-  (let [[a b c _ d e f] (s/split s #" ")]
-    [a
-     (condp = b
-       "inc" +
-       "dec" -)
-     (Integer. c)
-     d
-     (condp = e
-       ">" >
-       "<" <
-       ">=" >=
-       "==" =
-       "<=" <=
-       "!=" not=)
-     (Integer. f)]))
-
+(def opm {"inc" +
+          "dec" -
+          ">" >
+          "<" <
+          ">=" >=
+          "==" =
+          "<=" <=
+          "!=" not=})
 (def in
-  (mapv parse
+  (mapv #(let [[a b c _ d e f] (s/split % #" ")]
+           [a (get opm b) (Integer. c)
+            d (get opm e) (Integer. f)])
         (-> (slurp "resources/d8.txt")
             (s/split #"\n"))))
 
 (defn run []
   (println "q1" (q1 in))
   (println "q2" (q2 in)))
-
