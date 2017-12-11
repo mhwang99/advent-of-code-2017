9d886a791278cb8beb5a2dbdd050882563b06363
Mark Hwang <mhwang@serenova.com>
2017-12-11 01:47:01 -0400

    day 11

---
+++ src/aoc17/d11.clj
@@ -0,0 +1,41 @@
+(ns aoc17.d11
+  (:require [clojure.string :as s]))
+
+(defn steps [x y]
+  (loop [n 0
+         x (max x (- x))
+         y (max y (- y))]
+    (cond
+      (= x y 0) n
+      (= y 0) (recur (+ n 2) (- x 6) y)
+      (> x 0) (recur (inc n) (- x 3) (- y 2))
+      :else (recur (inc n) x (- y 4)))))
+
+(defn q0 [l]
+  (reduce (fn [[x y mx] e]
+            (let [[x y] (condp = e
+                          "n" [x (+ y 4)]
+                          "s" [x (- y 4)]
+                          "ne" [(+ x 3) (+ y 2)]
+                          "nw" [(- x 3) (+ y 2)]
+                          "sw" [(- x 3) (- y 2)]
+                          "se" [(+ x 3) (- y 2)])]
+              [x y (max mx (steps x y))]))
+          [0 0 0] l))
+
+(defn q1 [l]
+  (let [[x y] (q0 l)]
+    (steps x y)))
+
+(defn q2 [l]
+  (let [[_ _ mx] (q0 l)]
+    mx))
+
+(def in
+  (-> (slurp "resources/d11.txt")
+      (s/split #",|\n")))
+
+(defn run []
+  (println "q1" (q1 in))
+  (println "q2" (q2 in)))
+
