9f1f2d230b642f2b39e73dc83a248985c94f8977
Mark Hwang <mhwang@liveopscloud.com>
2017-12-13 11:55:11 -0400

    day 13

--- src/aoc17/d13.clj
+++ src/aoc17/d13.clj
@@ -11,20 +11,20 @@
 
 (defn q2
   [dm]
-  (loop [n 0]
-    (if (reduce (fn [_ [i d]]
-                  (when (= 0 (mod (+ i n)
-                                  (+ d (- d 2))))
-                    (reduced i)))
-                nil dm)
-      (recur (inc n))
-      n)))
+  (some (fn [n]
+          (when-not (some (fn [[i d]]
+                            (= 0 (mod (+ i n)
+                                      (+ d (- d 2)))))
+                          dm)
+            n))
+        (range)))
 
 (def in
-  (into {} (mapv (fn [l]
-          (mapv #(Integer. %) (re-seq #"\d+" l)))
-        (-> (slurp "resources/d13.txt")
-            (s/split #"\n")))))
+  (into {}
+        (mapv (fn [l]
+                (mapv #(Integer. %) (re-seq #"\d+" l)))
+              (-> (slurp "resources/d13.txt")
+                  (s/split #"\n")))))
 
 (defn run []
   (println "q1" (q1 in))
