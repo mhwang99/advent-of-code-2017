9b9c61e0965d2858a0a4cfde95f4f4665a3d987f
Mark Hwang <mhwang@liveopscloud.com>
2017-12-15 11:29:55 -0400

    day 15

--- src/aoc17/d15.clj
+++ src/aoc17/d15.clj
@@ -2,37 +2,34 @@
   (:require [clojure.string :as s]))
 
 (defn q0
-  [ab cnt f-gen gv]
+  [ab cnt gv]
   (loop [n 0
          ab ab
          ret 0]
     (if (= n cnt)
       ret
-      (let [[a b] (mapv f-gen ab [16807 48271] gv)]
+      (let [[a b] (mapv (fn [a u m]
+                          (reduce (fn [r v]
+                                    (let [r (mod (* r v) 2147483647)]
+                                      (if (= 0 (bit-and r (dec m)))
+                                        (reduced r)
+                                        r)))
+                                  a (repeat u)))
+                        ab [16807 48271] gv)]
         (if (= (bit-and a 65535) (bit-and b 65535))
           (recur (inc n) [a b] (inc ret))
           (recur (inc n) [a b] ret))))))
 
 (defn q1 [ab cnt]
-  (let [f-gen (fn [a u m]
-                (mod (* a u) 2147483647))]
-    (q0 ab cnt f-gen [0 0])))
+  (q0 ab cnt [1 1]))
 
 (defn q2 [ab cnt]
-  (let [f-gen (fn [a u m]
-                (reduce (fn [r v]
-                          (let [r (mod (* r v) 2147483647)]
-                            (if (= 0 (bit-and r (dec m)))
-                              (reduced r)
-                              r)))
-                        a (repeat u)))]
-    (q0 ab cnt f-gen [4 8])))
+  (q0 ab cnt [4 8]))
 
 (def in
   (->> (slurp "resources/d15.txt")
        (re-seq #"\d+" )
        (mapv #(Integer. %))))
-
 
 (defn run []
   (println "q1" (q1 in 40000000))
