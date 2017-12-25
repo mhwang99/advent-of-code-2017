907f65e8cfa6b8fa51a7e2e8fbea05aa8746fee5
Mark Hwang <mhwang@serenova.com>
2017-12-25 03:17:19 -0400

    day 25

--- src/aoc17/d25.clj
+++ src/aoc17/d25.clj
@@ -7,27 +7,23 @@
         n (Integer. (first (re-seq #"\d+" (first l))))
         s (keyword (nth (s/split (first l) #" |\.") 3))
         m (reduce (fn [m s]
-                    (let [l (mapv second (re-seq #" ([\w\d]+)[\.\:]\n?" s))]
+                    (let [l (mapv second (re-seq #" ([\w\d]+)[\.\:]\n?" s))
+                          gen (fn [l]
+                                {:fvl (if (= (nth l 0) "1") conj disj)
+                                 :mv (if (= "left" (nth l 1)) -1 1)
+                                 :nxt (keyword (nth l 2))})]
                       (assoc m (keyword (nth l 0))
-                             {false {:fvl (if (= (nth l 2) "1") conj disj)
-                                     :mv (if (= "left" (nth l 3)) -1 1)
-                                     :nxt (keyword (nth l 4))}
-                              true {:fvl (if (= (nth l 6) "1") conj disj)
-                                    :mv (if (= "left" (nth l 7)) -1 1)
-                                    :nxt (keyword (nth l 8))}}))
-                    )
+                             {false (gen (drop 2 l))
+                              true (gen (drop 6 l))})))
                   {} (rest l))]
     [s n m]))
 
 (defn q1 [[s n m]]
-   (loop [n n
-          p 0
-          s s
-          v #{}]
-     (if (<= n 0)
-       (count v)
-       (let [{:keys [fvl mv nxt]} (get-in m [s (boolean (v p))])]
-         (recur (dec n) (+ p mv) nxt (fvl v p))))))
+  (-> (reduce (fn [[v p s] _]
+                (let [{:keys [fvl mv nxt]} (get-in m [s (boolean (v p))])]
+                  [(fvl v p) (+ p mv) nxt]))
+              [#{} 0 s] (range n))
+      first count))
 
 (defn run []
   (println "q1" (q1 in)))
